6a5985c83f8c75bd5cfcba2900c96a1af5a98e9d
Laura Jackson <playeronereadystart@gmail.com>
2018-09-18 21:16:56 +0100

    Add ability to post names to new route

--- app.rb
+++ app.rb
@@ -5,4 +5,15 @@ class Battle < Sinatra::Base
   get '/' do
     'Testing infrastructure working!'
   end
+
+  get '/form' do
+    erb :index
+  end
+
+  post '/names' do
+    p params
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb :play
+  end
 end
--- spec/features/testing_home_spec.rb
+++ spec/features/testing_home_spec.rb
@@ -4,3 +4,12 @@ feature 'homepage' do
     expect(page).to have_content("Testing infrastructure working!")
   end
 end
+
+feature 'form' do
+  scenario 'checks forms can be filled in and submit and see names on screen' do
+    visit('/form')
+    fill_in('player1', with: 'Ash')
+    fill_in('player1', with: 'Misty')
+    click_button('Save')
+  end
+end
