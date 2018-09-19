21bc03e83bafdcf821a07e4c3389ba5202b3aa87
Laura Jackson <playeronereadystart@gmail.com>
2018-09-19 15:05:07 +0100

    Add Attack button

--- app.rb
+++ app.rb
@@ -12,13 +12,22 @@ class Battle < Sinatra::Base
     p params
     session[:player1] = params[:player1]
     session[:player2] = params[:player2]
+    session[:player1_hitpoints] = 50
+    session[:player2_hitpoints] = 50
     redirect '/play'
   end
 
   get '/play' do
+    @player1_hitpoints = session[:player1_hitpoints]
+    @player2_hitpoints = session[:player2_hitpoints]
     @player1 = session[:player1]
     @player2 = session[:player2]
     erb :play
+  end
+
+  post '/player1_attacks' do
+    session[:player2_hitpoints] -= 10
+    redirect '/play'
   end
 
   run! if app_file == $0
